465d123f3aac75fa0677f2115f70635a874ee06e
Harishm30 <117711949+Harishm30@users.noreply.github.com>
2023-06-10 09:56:15 +0530

    Update node (1).py
    Assignment 5 3rd answer

--- node (1).py
+++ node (1).py
@@ -1,14 +1,6 @@
-class Node():
-  def __init__(self,val):
-    self.val = val
-    self.next = next 
-s = Node(1,Node)
-print(s.val,s.next) 
-
-s2 = Node(2,Node)
-print(s.val,s.next) 
-s.next = s2
-print("Connected list",s.val,s.next)
-
-print(s.next.val)
-print(s.next.next)
+a=2
+b=2
+if(a%b==0):
+  print("a equal to b")
+else :
+  print("a is not equal to b")
